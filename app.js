7f2e988db66d7e84a6ef06fe60d108f9942b73ae
Paolo <paolo.ghidoni82@gmail.com>
2021-11-09 14:32:25 +0000

    Modify Js to make APIs interact - Relates #14
    Now the response data retrieved using fetch from the weather API
    is being used to retrieve data from the Giphy API, requesting
    a gif related to the weather description.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 /* TO DO:
 
-* Write function to randomize gif pictures selection from the retrieved array.
-
-
-*/
+* Decide what to do with the articles, andif we want them to load when the page loads
+* giphy : Can we retrieve the alt attribute? I want to always retrieve a random image, not always the same
+* Figure out if RoadGoat can be used with fetch
 
 /*-------------------------------------------*\
   VARIABLES
@@ -14,6 +13,7 @@ const userInputForm = document.querySelector(".user-input__form");
 const userInputDisplay = document.querySelector(".user-input__display");
 const searchBtn = document.querySelector(".user-input__btn");
 const cityNameDisplay = document.querySelector(".city-name-display");
+const cityInfoDisplay = document.querySelector(".user-input__city-info");
 
 /* Giphy API */
 const gifSection = document.querySelector(".gif");
@@ -25,125 +25,96 @@ const currentTemperature = document.querySelector(".weather__current-temp");
 const weatherDescription = document.querySelector(".weather__current-temp");
 
 /* news */
+const articleBody = document.querySelector('.article__body');
 const currentNews = document.querySelector(".news");
 const newsKey = "070c988ee1c2436c95535e3a38969fc5";
-/*-------------------------------------------*\
-  Giphy API - retrieves images
-\*-------------------------------------------*/
-
-// Can we retrieve the alt attribute?
-// I want to always retrieve a random image, not always the same.
-
-/* Function to display data */
-
-const displayImage = data => {
-  // Create html to update DOM
-  const html = `
-
-  <img src="${data.data[0].images.downsized.url}" alt="" class='gif__img'>
-
-  `;
-
-  // update DOM
-
-  gifSection.firstElementChild.innerHTML = html;
-
-}
 
 
-// /* API request */
 
-// fetch(`https://api.giphy.com/v1/gifs/search?q=${imgSearchKeyword}&api_key=${giphyKey}`)
-//   .then(response => response.json())
-// // .then(data => console.log(data.data[0].images.downsized.url))
-//   .then(data => displayImage(data))
 
-/*-------------------------------------------*\
-  goweather API - retrieves weather forecast
-\*-------------------------------------------*/
-
-/* Function to display data */
-function displayWeather(data) {
-  // Create html to update DOM
-  const html = `
-  <div class="weather-section__description">
-    <p>${data.description}</p>
-  </div>
-
-  <div class="weather-section__current-temp">
-    <p>${data.temperature}</p>
-  </div>
-
-  `;
-
-  // update DOM
-
-  weatherSection.firstElementChild.innerHTML = html;
-
-}
-
-// /* API request */
-// fetch('https://goweather.herokuapp.com/weather/spain')
-//   .then(response => response.json())
-//   .then(data => displayWeather(data));
-
-/*----------------------------------------------------*\
-  Road Goat - retrieves interesting info about cities
-\*----------------------------------------------------*/
 
 /*----------------------------------------------------*\
   APP FUNCTIONS
 \*----------------------------------------------------*/
 
-// Function to render the user city selection on the page
-function displayCityName(inputCity) {
-
+/* Function to render the user city selection on the page */
+const displayCityName = inputCity => {
   // Create a paragraph to display the city name
   inputCityName = `<P>${inputCity}</P>`.toUpperCase();
-  
   // Display the city name
   userInputDisplay.innerHTML = inputCityName; 
-
 }
+
+
+/* Function to display temperature and weather description from goweather API */
+const displayWeather = data => {
+  // Create html to update DOM
+  const html = `
+  <div class="weather-section__description">
+    <p>${data.description}</p>
+  </div>
+  <div class="weather-section__current-temp">
+    <p>${data.temperature}</p>
+  </div>
+  `;
+  // update DOM
+  weatherSection.firstElementChild.innerHTML = html;
+}
+
+
+/* Function to display gif from Giphy API */
+const displayGif = data => {
+  // Create html to update DOM
+  const html = `<img src="${data.data[0].images.downsized.url}" alt="" class='gif__img'>`;
+  // update DOM
+  gifSection.firstElementChild.innerHTML = html;
+}
+
+
+/* Function to display articles from the world from newsapi */
+const displayArticle = data => {
+  const html = `<P>${data.articles[0].content}</P>`;
+  // update DOM
+  articleBody.innerHTML = html;
+}
+
 
 // Function to update ALL data on the page relative to the city
 function updateCityData() {
   // Retrieve value entered by user in input field
   const inputCity = document.querySelector("#user-input__input--city").value;
-
   // Call displayCityName() to display the city name
   displayCityName(inputCity);
 
-  /* Giphy */
-
-  fetch(
-    `https://api.giphy.com/v1/gifs/search?q=${inputCity}&api_key=${giphyKey}`
-  )
+  
+/* News */
+  fetch(`https://newsapi.org/v2/everything?q=tesla&from=2021-11-09&sortBy=popularity&apiKey=${newsKey}`)
     .then(response => response.json())
+    // .then(data => console.log(data.articles[0].content))
+    .then(data => displayArticle(data));
+
+
+  /* API requests */
+  fetch(`https://goweather.herokuapp.com/weather/${inputCity}`) // Fetch weather data from goweather
+    .then(response => response.json())
+    .then(data => {
+      displayWeather(data); // Display the temperature and the weather description
+      return fetch(`https://api.giphy.com/v1/gifs/search?q=${data.description}&api_key=${giphyKey}`) // Query the giphy API for a gif using the weather description
+    })
+    .then(response => response.json()) // Parse the data received from giphy to Json
     // .then(data => console.log(data.data[0].images.downsized.url))
-    .then(data => displayImage(data));
-
-  /* Weather */
-
-  /* API request */
-  fetch(`https://goweather.herokuapp.com/weather/${inputCity}`)
-    .then(response => response.json())
-    .then(data => displayWeather(data));
-
-  /* News */
-
-  fetch(
-    `https://newsapi.org/v2/everything?q=tesla&from=2021-11-09&sortBy=publishedAt&apiKey=${newsKey}`
-  )
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => displayGif(data)) // Display the image
+    .catch(error => console.log(error));
 }
+
+
+
+
+
 
 /*----------------------------------------------------*\
   EVENT LISTENERS
 \*----------------------------------------------------*/
-
-// WE NEE TO CHAIN ALL APIs requests and make them dynamic depending on the user input
 
 // When user clicks button, update all data and render it on the page.
 searchBtn.addEventListener("click", updateCityData);
