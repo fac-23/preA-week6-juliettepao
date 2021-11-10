4e62036201efbb0045e43a50972095b354b1ca83
Paolo <paolo.ghidoni82@gmail.com>
2021-11-10 12:23:29 +0000

    Integrate unsplash API, implement Promise.all, refactor code - #14
    * Integrated unsplash API to retrieve image of city.
    * Implemented Promise.all to simultaneously fetch content from both
    unsplash API and News API.
    * Chained fetch http requests of Goweather API and Giphy API so that
    the data retrieved by giphy is dinamically related to the data
    previously retrieved by Goweather.
    * Refactored and commented code.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* TO DO:
 
-* Decide what to do with the articles, andif we want them to load when the page loads
-* giphy : Can we retrieve the alt attribute? I want to always retrieve a random image, not always the same
+* Decide what to do with the articles, and if we want them to load when the page loads
+* giphy & unsplash : I want to always retrieve a random image, not always the same
 * Figure out if RoadGoat can be used with fetch
 
 /*-------------------------------------------*\
@@ -15,8 +15,11 @@ const searchBtn = document.querySelector(".user-input__btn");
 const cityNameDisplay = document.querySelector(".city-name-display");
 const cityInfoDisplay = document.querySelector(".user-input__city-info");
 
+/* Unsplash api */
+const unsplashKey = 'ESaaep_nogLd-aU7_GUQL8Tn_aKGF-YxLkRMLfoDJdU';
+
 /* Giphy API */
-const gifSection = document.querySelector(".gif");
+const cityPicSection = document.querySelector(".city-pic");
 const giphyKey = "RXsaSeN2Q8Sm1V12ZBhek7ZesgLCWrFY";
 
 /* goweather API */
@@ -34,7 +37,7 @@ const newsKey = "070c988ee1c2436c95535e3a38969fc5";
 
 
 /*----------------------------------------------------*\
-  APP FUNCTIONS
+  APP FUNCTIONS TO DISPLAY FETCHED CONTENT
 \*----------------------------------------------------*/
 
 /* Function to render the user city selection on the page */
@@ -65,45 +68,74 @@ const displayWeather = data => {
 /* Function to display gif from Giphy API */
 const displayGif = data => {
   // Create html to update DOM
-  const html = `<img src="${data.data[0].images.downsized.url}" alt="" class='gif__img'>`;
+  const weatherGif = document.createElement('img');
+  weatherGif.classList.add('weather__img');
+  weatherGif.alt = `${data.data[0].title}`
+  weatherGif.src = `${data.data[0].images.downsized.url}`;
   // update DOM
-  gifSection.firstElementChild.innerHTML = html;
+  weatherSection.firstElementChild.append(weatherGif);
+}
+
+
+/* Function to display a picture of the city from Unsplash */
+const displayCityPic = data => {
+  // Create html to update DOM
+  const cityPic = `<img src="${data.results[0].urls.small}" alt="${data.results[0].alt_description}" class="city-pic__img">`;
+
+  // update DOM
+  cityPicSection.firstElementChild.innerHTML = cityPic;
 }
 
 
 /* Function to display articles from the world from newsapi */
 const displayArticle = data => {
-  const html = `<P>${data.articles[0].content}</P>`;
+  // Create html to update DOM
+  const html = `
+  <h2>News</h2>
+  <P>${data.articles[0].content}</P>`;
   // update DOM
   articleBody.innerHTML = html;
 }
 
 
-// Function to update ALL data on the page relative to the city
+// Function to update ALL data on the page relative to the city, once the user clicks the button.
 function updateCityData() {
-  // Retrieve value entered by user in input field
+  // Retrieve value entered by user in input field.
   const inputCity = document.querySelector("#user-input__input--city").value;
-  // Call displayCityName() to display the city name
+  // Call displayCityName() to display the city name.
   displayCityName(inputCity);
 
-  
-/* News */
-  fetch(`https://newsapi.org/v2/everything?q=tesla&from=2021-11-09&sortBy=popularity&apiKey=${newsKey}`)
-    .then(response => response.json())
-    // .then(data => console.log(data.articles[0].content))
-    .then(data => displayArticle(data));
+  // Use promiseAll to simultaneously retrieve both unsplash API and news API data.
+  const unsplashPromise = fetch(`https://api.unsplash.com/search/photos/?client_id=${unsplashKey}&query?page=1&query=${inputCity}`);
+
+  const newsApiPromise =  fetch(`https://newsapi.org/v2/everything?q=${inputCity}&from=2021-11-09&sortBy=popularity&apiKey=${newsKey}`);
 
 
-  /* API requests */
-  fetch(`https://goweather.herokuapp.com/weather/${inputCity}`) // Fetch weather data from goweather
-    .then(response => response.json())
-    .then(data => {
-      displayWeather(data); // Display the temperature and the weather description
-      return fetch(`https://api.giphy.com/v1/gifs/search?q=${data.description}&api_key=${giphyKey}`) // Query the giphy API for a gif using the weather description
+  Promise.all([unsplashPromise, newsApiPromise])
+    .then(apisResponsesArray => {
+      // Return another promise as an array with the parsed content of the previous response.
+      return Promise.all(apisResponsesArray.map(apiResponse => apiResponse.json()));
     })
-    .then(response => response.json()) // Parse the data received from giphy to Json
-    // .then(data => console.log(data.data[0].images.downsized.url))
-    .then(data => displayGif(data)) // Display the image
+    .then(parsedReponsesArray => {
+      // Destructure the parsed reponses array into separate variables.
+      const [unsplashResponse, newsApiResponse] = parsedReponsesArray; 
+
+      // Call functions to display content of the separate responses of the APIs.
+      displayCityPic(unsplashResponse);
+      displayArticle(newsApiResponse)
+    })
+    .catch(error => console.log(error));
+
+
+  /* Chain Goweather API and Giphy API requests */
+  fetch(`https://goweather.herokuapp.com/weather/${inputCity}`) // Fetch weather data from Goweather.
+    .then(response => response.json())
+    .then(weather => {
+      displayWeather(weather); // Display the temperature and the weather description
+      return fetch(`https://api.giphy.com/v1/gifs/search?q=${weather.description}&api_key=${giphyKey}`) // return a promise to Query the giphy API for a gif using the weather description.
+    })
+    .then(response => response.json()) // Parse the data received from giphy to Json.
+    .then(gifContent => displayGif(gifContent)) // Display the gif image.
     .catch(error => console.log(error));
 }
 
