a31ac5a1f539e6f103e9a6ee6472c6faf0797903
Paolo <paolo.ghidoni82@gmail.com>
2021-11-20 01:58:04 +0000

    Fix js
    * Error when weather can not be retrieved is now displayed in
    the correct div, the weather div.
    * Cleaned unused variables / code

--- app.js
+++ app.js
@@ -9,65 +9,65 @@
 \*-------------------------------------------*/
 
 /* User Input section */
-const userInputForm = document.querySelector("form");
-const userInputDisplay = document.querySelector(".user-input__display");
-const searchBtn = document.querySelector(".user-input__btn");
-const cityNameDisplay = document.querySelector(".city-name-display");
-const cityInfoDisplay = document.querySelector(".user-input__city-info");
+const userInputForm = document.querySelector('form');
+const userInputDisplay = document.querySelector('.user-input__display')
+const searchBtn = document.querySelector('.user-input__btn')
+const cityNameDisplay = document.querySelector('.city-name-display')
+const cityInfoDisplay = document.querySelector('.user-input__city-info')
 
 /* Unsplash api */
-const unsplashKey = 'ESaaep_nogLd-aU7_GUQL8Tn_aKGF-YxLkRMLfoDJdU';
-const cityPicSection = document.querySelector(".city-pic");
+const unsplashKey = 'ESaaep_nogLd-aU7_GUQL8Tn_aKGF-YxLkRMLfoDJdU'
+const cityPicSection = document.querySelector('.city-pic')
 
 /* Giphy API */
-const giphyKey = "RXsaSeN2Q8Sm1V12ZBhek7ZesgLCWrFY";
+const giphyKey = 'RXsaSeN2Q8Sm1V12ZBhek7ZesgLCWrFY'
 
 /* goweather API */
-const weatherDescriptionDiv = document.querySelector(".weather__description");
-const weatherImageDiv = document.querySelector(".weather__gif");
-const weatherDescription = document.querySelector(".weather__description");
+const weatherDescriptionDiv = document.querySelector('.weather__description')
+const weatherDisplay = document.querySelector('.weather')
+const weatherImageDiv = document.querySelector('.weather__gif')
 
 /* Guardian API */
-const articleBody = document.querySelector('.article__body');
-const currentNews = document.querySelector(".news");
-const newsKey = "ffb6c45e-9bcc-4828-b865-e4f13ac02107";
-
-
-
-
+const articleBody = document.querySelector('.article__body')
+const currentNews = document.querySelector('.news')
+const newsKey = 'ffb6c45e-9bcc-4828-b865-e4f13ac02107'
 
 /*----------------------------------------------------*\
   APP FUNCTIONS TO DISPLAY FETCHED CONTENT
 \*----------------------------------------------------*/
 
 /* Function to generate random number for different APIs */
-const randomNumber = (number) => Math.floor(Math.random() * (number - 1));
+const randomNumber = (number) => Math.floor(Math.random() * (number - 1))
 
 /* Function to display errors */
 const displayError = (errorMessage) => {
-  return fetch (`https://api.giphy.com/v1/gifs/search?q=error&api_key=${giphyKey}`)
-    .then(response => response.json())
-    .then(data => {
+  return fetch(
+    `https://api.giphy.com/v1/gifs/search?q=error&api_key=${giphyKey}`,
+  )
+    .then((response) => response.json())
+    .then((data) => {
       const html = `
       <p>${errorMessage}</p>
-      <img src="${data.data[randomNumber(data.data.length)].images.downsized.url}" alt="${data.data[randomNumber(data.data.length)].title}" class="weather__img">`;
-      // weatherImageDiv.innerHTML = html;
-      cityPicSection.innerHTML = html;
-  })
+      <img src="${
+        data.data[randomNumber(data.data.length)].images.downsized.url
+      }" alt="${
+        data.data[randomNumber(data.data.length)].title
+      }" class="weather__img">`
+      weatherDisplay.innerHTML = html
+      // cityPicSection.innerHTML = html;
+    })
 }
-
 
 /* Function to render the user city selection on the page */
-const displayCityName = data => {
+const displayCityName = (data) => {
   // Create a paragraph to display the city name
-  html = `<P>${data}</P>`.toUpperCase();
+  html = `<P>${data}</P>`.toUpperCase()
   // Display the city name
-  userInputDisplay.innerHTML = html; 
+  userInputDisplay.innerHTML = html
 }
 
-
 /* Function to display temperature and weather description from goweather API */
-const displayWeather = data => {
+const displayWeather = (data) => {
   // Create html to update DOM
   const html = `
   <h2>Weather</h2><br>
@@ -77,132 +77,140 @@ const displayWeather = data => {
   <div class="weather-section__current-temp">
     <p>${data.temperature}</p>
   </div>
-  `;
+  `
   // update DOM
-  weatherDescriptionDiv.innerHTML = html;
+  weatherDescriptionDiv.innerHTML = html
 }
-
 
 /* Function to display gif from Giphy API */
-const displayGif = data => {
+const displayGif = (data) => {
   // Create html to update DOM
   const html = `
-  <img src="${data.data[randomNumber(data.data.length)].images.downsized.url}" alt="${data.data[randomNumber(data.data.length)].title}" class="weather__img">`;
+  <img src="${
+    data.data[randomNumber(data.data.length)].images.downsized.url
+  }" alt="${
+    data.data[randomNumber(data.data.length)].title
+  }" class="weather__img">`
 
   // update DOM
-  weatherImageDiv.innerHTML = html;
+  weatherImageDiv.innerHTML = html
 }
-
 
 /* Function to display a picture of the city from Unsplash */
-const displayCityPic = data => {
+const displayCityPic = (data) => {
   // Create html to update DOM
-  
-  const html = `<img src="${data.results[randomNumber(data.results.length)].urls.small}" alt="${data.results[randomNumber(data.results.length)].alt_description}" class="city-pic__img">`;
+
+  const html = `<img src="${
+    data.results[randomNumber(data.results.length)].urls.small
+  }" alt="${
+    data.results[randomNumber(data.results.length)].alt_description
+  }" class="city-pic__img">`
 
   // update DOM
-  cityPicSection.innerHTML = html;
+  cityPicSection.innerHTML = html
 }
 
-
 /* Function to display random articles from the city from newsapi */
-const displayArticle = data => {
+const displayArticle = (data) => {
   // Create html to update DOM
 
   const html = `
   <h2>News</h2>
-  <P>${data.response.results[randomNumber(data.response.results.length)].webTitle}</P>
-  <a href="${data.response.results[randomNumber(data.response.results.length)].webUrl}" target="_blank">Read the full article</a>
-  `;
+  <P>${
+    data.response.results[randomNumber(data.response.results.length)].webTitle
+  }</P>
+  <a href="${
+    data.response.results[randomNumber(data.response.results.length)].webUrl
+  }" target="_blank">Read the full article</a>
+  `
   // update DOM
-  articleBody.innerHTML = html;
+  articleBody.innerHTML = html
 }
-
-
 
 // Function to update ALL data on the page relative to the city, once the user clicks the button.
 function updateCityData(cityName) {
   // Call displayCityName() to display the city name.
-  displayCityName(cityName);
+  displayCityName(cityName)
 
   // Use promiseAll to simultaneously retrieve both unsplash API and news API data.
-  const unsplashPromise = fetch(`https://api.unsplash.com/search/photos/?client_id=${unsplashKey}&query?page=1&query=${cityName}`);
+  const unsplashPromise = fetch(
+    `https://api.unsplash.com/search/photos/?client_id=${unsplashKey}&query?page=1&query=${cityName}`,
+  )
 
   // const newsApiPromise =  fetch(`https://content.guardianapis.com/search?q=${cityName}&api-key=${newsKey}`);
-  const newsApiPromise =  fetch(`https://content.guardianapis.com/search?q=${cityName}%20AND%20travel&api-key=${newsKey}`);
-
+  const newsApiPromise = fetch(
+    `https://content.guardianapis.com/search?q=${cityName}%20AND%20travel&api-key=${newsKey}`,
+  )
 
   Promise.all([unsplashPromise, newsApiPromise])
-    .then(apisResponsesArray => {
+    .then((apisResponsesArray) => {
       // Return another promise as an array with the parsed content of the previous response.
-      return Promise.all(apisResponsesArray.map(apiResponse => apiResponse.json()));
+      return Promise.all(
+        apisResponsesArray.map((apiResponse) => apiResponse.json()),
+      )
     })
-    .then(parsedReponsesArray => {
+    .then((parsedReponsesArray) => {
       // Destructure the parsed reponses array into separate variables.
-      const [unsplashResponse, newsApiResponse] = parsedReponsesArray; 
+      const [unsplashResponse, newsApiResponse] = parsedReponsesArray
 
       // Call functions to display content of the separate responses of the APIs.
-      displayCityPic(unsplashResponse);
+      displayCityPic(unsplashResponse)
       displayArticle(newsApiResponse)
     })
-    .catch(error => console.log(error));
-
+    .catch((error) => console.log(error))
 
   /* Chain Goweather API and Giphy API requests */
   fetch(`https://goweather.herokuapp.com/weather/${cityName}`) // Fetch weather data from Goweather.
-    .then(response =>  {
-      if(!response.ok) {
-        const error = new Error(response.status);
-        throw error;
+    .then((response) => {
+      if (!response.ok) {
+        const error = new Error(response.status)
+        throw error
       }
-      return response.json()})
-    .then(weather => {
-      displayWeather(weather); // Display the temperature and the weather description
-      return fetch(`https://api.giphy.com/v1/gifs/search?q=${weather.description}&api_key=${giphyKey}`) // return a promise to Query the giphy API for a gif using the weather description.
+      return response.json()
     })
-    .then(response => response.json()) // Parse the data received from giphy to Json.
-    .then(gifContent => displayGif(gifContent)) // Display the gif image.
-    .catch(error => {
-      console.log(error);
-      if(error.message === '404') {
-        let message = "Couldn't retrieve weather for the requested city.";
-        displayError(message);
+    .then((weather) => {
+      displayWeather(weather) // Display the temperature and the weather description
+      return fetch(
+        `https://api.giphy.com/v1/gifs/search?q=${weather.description}&api_key=${giphyKey}`,
+      ) // return a promise to Query the giphy API for a gif using the weather description.
+    })
+    .then((response) => response.json()) // Parse the data received from giphy to Json.
+    .then((gifContent) => displayGif(gifContent)) // Display the gif image.
+    .catch((error) => {
+      console.log(error)
+      if (error.message === '404') {
+        let message = "Couldn't retrieve weather for the requested city."
+        displayError(message)
       } else {
-        let message = "Something DEFINITELY went wrong here. We could not find that city 🤔";
-        displayError(message);
+        let message =
+          'Something DEFINITELY went wrong here. We could not find that city 🤔'
+        displayError(message)
       }
-    });
+    })
 }
-
-
-console.log(weatherDescription);
-
-
 
 /*----------------------------------------------------*\
   EVENT LISTENERS
 \*----------------------------------------------------*/
 
-
 // When user presses enter on keyboard, get the submitted data from the form, call updateCityData() passing the city name as argument to update all data and render it on the page.
 window.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
-    const formData = new FormData(userInputForm);
+    const formData = new FormData(userInputForm)
     // Retrieve value entered by user in input field.
-    const cityName = formData.get("user-input__input--city");
+    const cityName = formData.get('user-input__input--city')
     // Update data on page.
-    updateCityData(cityName);
+    updateCityData(cityName)
   }
-});
-
+})
 
 // When user clicks button, get the submitted data from the form, call updateCityData() passing the city name as argument to update all data and render it on the page.
-userInputForm.addEventListener('submit', e => {
-  e.preventDefault(); // prevents the form from submitting automatically
+userInputForm.addEventListener('submit', (e) => {
+  e.preventDefault() // prevents the form from submitting automatically
   // FormData interface mirrors a form's native behaviour
-  const formData = new FormData(userInputForm);
+  const formData = new FormData(userInputForm)
   // Retrieve value entered by user in input field.
-  const cityName = formData.get("user-input__input--city");
+  const cityName = formData.get('user-input__input--city')
   // Update data on page.
-  updateCityData(cityName);
-});
+  updateCityData(cityName)
+})
